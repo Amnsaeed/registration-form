1f52b8c269ae5584c30bfe1168dde3197bcebe8f
Your Name <you@example.com>
2023-10-31 16:24:47 +0100

    second commit

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+const Navbar = () => {
+  return ( 
+
+    <div className="navbar">
+
+      <h1>Registration Form</h1>
+
+      <div className="button-bar">
+
+      <a className="home-button" href="/"> Home </a>
+      <a href="/create"> Submit New Form </a>
+
+
+      </div>
+
+    </div>
+
+
+   );
+}
+ 
+export default Navbar;
